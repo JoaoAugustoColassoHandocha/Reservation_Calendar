93e970a320bc0f95cc764a77b66828ab12a4b65f
João Augusto Colasso Handocha <joaoaugustocolassohandocha@gmail.com>
2025-05-27 11:12:07 -0300

    Update darkmode.js

--- assets/Scripts/darkmode.js
+++ assets/Scripts/darkmode.js
@@ -1,11 +1,7 @@
 const html = document.querySelector('html')
 const checkbox = document.querySelector("input[name=theme]")
 
-const getStyle = (element, style) =>
-  window
-    .getComputedStyle(element)
-    .getPropertyValue(style)
-
+const getStyle = (element, style) => window.getComputedStyle(element).getPropertyValue(style)
 
 const initialColors = {
   bodyColor: getStyle(html, '--body-color'),
